f3f8b79e532015338db7f8e2fb3d3ceba1738de1
Luis Bejarano <l.bejarano@globant.com>
2021-12-06 10:52:24 -0500

    buildSrc

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,16 @@
+package com.ui.compose
+
+object Dep {
+    object Versions {
+        object Compose {
+            const val Core = "1.0.4"
+            const val Foundation = "1.1.0-alpha-06"
+            const val Navigation = "2.4.0-alpha10"
+            const val RunTime = "1.1.0-alpha06"
+            const val LifeCycle = "1.0.0-alpha07"
+            const val Activity = "1.3.1"
+        }
+    }
+
+
+}
